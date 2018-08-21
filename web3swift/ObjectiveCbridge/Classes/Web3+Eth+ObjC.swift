1fca5f75de6d1b8e352fdf6d111b90067ea1ac8d
Anton Grigoriev <antongrigorjev2010@gmail.com>
2018-08-21 12:22:30 +0300

    tried objc bridjing

--- web3swift/ObjectiveCbridge/Classes/Web3+Eth+ObjC.swift
+++ web3swift/ObjectiveCbridge/Classes/Web3+Eth+ObjC.swift
@@ -34,4 +34,34 @@ public final class _ObjCweb3Eth: NSObject {
             return nil
         }
     }
+    
+    public func getBlockNumber(error: NSErrorPointer) -> _ObjCBigUInt? {
+        guard let result = self.web3?.eth.getBlockNumber() else {
+            error?.pointee = Web3Error.inputError("Web3 object was not properly initialized") as NSError
+            return nil
+        }
+        switch result {
+        case .success(let blockNumber):
+            let biguint = _ObjCBigUInt(value: blockNumber)
+            return biguint
+        case .failure(let web3error):
+            error?.pointee = web3error as NSError
+            return nil
+        }
+    }
+    
+    public func getGasPrice(error: NSErrorPointer) -> _ObjCBigUInt? {
+        guard let result = self.web3?.eth.getGasPrice() else {
+            error?.pointee = Web3Error.inputError("Web3 object was not properly initialized") as NSError
+            return nil
+        }
+        switch result {
+        case .success(let blockNumber):
+            let biguint = _ObjCBigUInt(value: blockNumber)
+            return biguint
+        case .failure(let web3error):
+            error?.pointee = web3error as NSError
+            return nil
+        }
+    }
 }
