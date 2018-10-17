23ca94bf8533959c6152640165dc7bcc7fd94706
Tomasz Mędryk <tomasz.medryk@10clouds.com>
2018-10-17 20:12:02 +0200

    Fix Carthage support

---
+++ web3swift-iOS-v2/web3swift_iOS_v2.h
@@ -0,0 +1,19 @@
+//
+//  web3swift_iOS_v2.h
+//  web3swift-iOS-v2
+//
+//  Created by Tomasz Mędryk on 17/10/2018.
+//  Copyright © 2018 Bankex Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for web3swift_iOS_v2.
+FOUNDATION_EXPORT double web3swift_iOS_v2VersionNumber;
+
+//! Project version string for web3swift_iOS_v2.
+FOUNDATION_EXPORT const unsigned char web3swift_iOS_v2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <web3swift_iOS_v2/PublicHeader.h>
+
+
